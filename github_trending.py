6fce94fb81e14db2c46bdac6d47b68da40214769
impu1se <impu1se@bk.ru>
2017-06-29 17:12:02 +0300

    'Finished'

--- github_trending.py
+++ github_trending.py
@@ -1,25 +1,34 @@
 import requests
-import time
+import sys
 
-url = 'https://api.github.com/search/repositories?q=created:>2017-06-10&sort=stars&order=desc'
+url = 'https://api.github.com/search/repositories?q=created:>2017-06-21&sort=stars&order=desc'
+
+
+def check_connect(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r
+    else:
+        return sys.exit('You have the problem to connect...')
+
 
 def get_trending_repositories(top_size):
     top_repo = []
     for repo in top_size['items']:
-        top_repo.append((repo['html_url'], repo['stargazers_count']))
-    return top_repo        
+        top_repo.append((repo['html_url'], repo['open_issues_count']))
+    return top_repo[:20]
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def printing_result(top_repo):
+    for repo in top_repo:
+        print('Url: {0} \t Open issues: {1}'.format(repo[0], repo[1]))
 
 
 def main():
-    r = requests.get(url)
-    top = get_trending_repositories(r.json())
-    print(top)
+    response = check_connect(url)
+    top = get_trending_repositories(response.json())
+    print_result(top)
 
 
 if __name__ == '__main__':
-	main()
-    
+    main()
